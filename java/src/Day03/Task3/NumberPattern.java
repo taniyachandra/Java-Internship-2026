ae5e3a270030116914d680e2ca4af780361fe624
taniyachandra <taniyachandra910@gmail.com>
2026-06-18 05:06:29 -0700

    Addes new files

---
+++ java/src/Day03/Task3/NumberPattern.java
@@ -0,0 +1,14 @@
+package Task3;
+
+public public class NumberPattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+} {
+    
+}
